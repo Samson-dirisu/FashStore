5173c5f192901507e5b5556342ec72d7f32b6f79
Samson-dirisu <56192333+Samson-dirisu@users.noreply.github.com>
2021-01-28 10:33:21 +0100

    fixed loged in issue

--- lib/components/database/user.dart
+++ lib/components/database/user.dart
@@ -5,12 +5,12 @@ class UserServices {
   String ref = "users";
 
   // CREATING A NEW NODE AND THE NAME OF THE NODE IS USERS. NODE ALSO CALLED TABLE IS SQL
-  createUser(Map value) {
-    _database
-        .reference()
-        .child(ref)
-        .push()
-        .set(value)
-        .catchError((e) => print(e.toString()));
-  }
+  createUser(Map value) => {
+        _database
+            .reference()
+            .child(ref)
+            .push()
+            .set(value)
+            .catchError((e) => print(e.toString()))
+      };
 }
