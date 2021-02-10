e873873a79dfd280b1f4372e8f96a2821a2b0f33
Samson-dirisu <56192333+Samson-dirisu@users.noreply.github.com>
2021-02-10 23:14:38 +0100

    used provider to handle business logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,41 @@
+import 'package:FashStore/provider/user_provider.dart';
+import 'package:FashStore/screens/home_page.dart';
 import 'package:FashStore/screens/login.dart';
+import 'package:FashStore/screens/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => UserProvider.initialize(),
+      child: MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.pink.shade400
-        ),
-        home: Login());
+        theme: ThemeData(primaryColor: Colors.pink.shade400),
+        home: ScreenController()),
+    )
+  );
+}
+
+class ScreenController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context);
+    switch(user.status) {
+      case Status.Uninitialized: 
+        return Splash();
+      case Status.Unauthenticated: 
+      case Status.Authenticating:
+        return Login();
+      case Status.Authenticated:
+        return HomePage();
+      default: return Login();
+    }
   }
 }
+
+
